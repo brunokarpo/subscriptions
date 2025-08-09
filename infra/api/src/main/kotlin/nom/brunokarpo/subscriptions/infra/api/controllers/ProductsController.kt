1edc9dc7fe5f151c838a6eb83a9d76c72855b996
Bruno Nogueira <ti.brunonogueira@gmail.com>
2025-08-09 18:21:13 -0300

    Handle `ProductUniqueNameException` in `ProductsController`, returning a `BAD_REQUEST` response. Add corresponding test coverage.

--- infra/api/src/main/kotlin/nom/brunokarpo/subscriptions/infra/api/controllers/ProductsController.kt
+++ infra/api/src/main/kotlin/nom/brunokarpo/subscriptions/infra/api/controllers/ProductsController.kt
@@ -1,9 +1,12 @@
 package nom.brunokarpo.subscriptions.infra.api.controllers
 
 import nom.brunokarpo.subscriptions.application.product.CreateNewProductUseCase
+import nom.brunokarpo.subscriptions.application.product.exceptions.ProductUniqueNameException
 import nom.brunokarpo.subscriptions.infra.api.dtos.CreateProductDtoRequest
 import nom.brunokarpo.subscriptions.infra.api.dtos.ProductDtoResponse
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -30,5 +33,12 @@ class ProductsController(
 
 		return ResponseEntity.status(201).body(productDtoResponse)
 	}
+
+	@ExceptionHandler(ProductUniqueNameException::class)
+	suspend fun handleProductUniqueNameException(ex: ProductUniqueNameException): ResponseEntity<Map<String, String>> = ResponseEntity.status(
+		HttpStatus.BAD_REQUEST
+	).body(
+		mapOf("message" to ex.message!!)
+	)
 
 }
--- infra/api/src/test/kotlin/nom/brunokarpo/subscriptions/infra/api/controllers/ProductsControllerTest.kt
+++ infra/api/src/test/kotlin/nom/brunokarpo/subscriptions/infra/api/controllers/ProductsControllerTest.kt
@@ -3,6 +3,7 @@ package nom.brunokarpo.subscriptions.infra.api.controllers
 import io.mockk.coEvery
 import kotlinx.coroutines.test.runTest
 import nom.brunokarpo.subscriptions.application.product.CreateNewProductUseCase
+import nom.brunokarpo.subscriptions.application.product.exceptions.ProductUniqueNameException
 import nom.brunokarpo.subscriptions.infra.api.ApiConfigurationTest
 import nom.brunokarpo.subscriptions.infra.api.dtos.CreateProductDtoRequest
 import org.junit.jupiter.api.Test
@@ -33,6 +34,24 @@ class ProductsControllerTest: ApiConfigurationTest() {
 			.expectBody()
 			.jsonPath("$.name").isEqualTo(expectedProductName)
 			.jsonPath("$.id").isEqualTo(expectedId)
+	}
 
+	@Test
+	fun `should return a handled error when create new product use case throw exception`() = runTest {
+		val expectedId = "79e9eb45-2835-49c8-ad3b-c951b591bc7f"
+		val expectedProductName = "Product 1"
+
+		coEvery { createProductUseCase.execute(any()) } throws ProductUniqueNameException(expectedProductName)
+
+		val productDto = CreateProductDtoRequest(name = expectedProductName)
+
+		client.post()
+			.uri("/v1/products")
+			.accept(MediaType.APPLICATION_JSON)
+			.bodyValue(productDto)
+			.exchange()
+			.expectStatus().isBadRequest
+			.expectBody()
+			.jsonPath("$.message").isEqualTo("Product with name '$expectedProductName' already exists!")
 	}
 }
