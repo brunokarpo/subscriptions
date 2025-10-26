7fb180db9653e5cfad10d2097ab554f33c1d453a
Bruno Nogueira <ti.brunonogueira@gmail.com>
2025-10-26 13:19:31 -0300

    Add ActivateSubscriptionUseCase bean configuration
    - Registered `ActivateSubscriptionUseCase` as a Spring bean in `UseCaseConfigurations`.

--- infra/app/src/main/kotlin/nom/brunokarpo/subscriptions/infra/app/configurations/UseCaseConfigurations.kt
+++ infra/app/src/main/kotlin/nom/brunokarpo/subscriptions/infra/app/configurations/UseCaseConfigurations.kt
@@ -2,6 +2,7 @@ package nom.brunokarpo.subscriptions.infra.app.configurations
 
 import nom.brunokarpo.subscriptions.application.customer.CreateNewCustomerUseCase
 import nom.brunokarpo.subscriptions.application.customer.ActivateCustomerUseCase
+import nom.brunokarpo.subscriptions.application.customer.ActivateSubscriptionUseCase
 import nom.brunokarpo.subscriptions.application.customer.RetrieveSubscriptionsByStatusUseCase
 import nom.brunokarpo.subscriptions.application.customer.SubscribeProductToCustomerUseCase
 import nom.brunokarpo.subscriptions.application.product.CreateNewProductUseCase
@@ -32,4 +33,8 @@ class UseCaseConfigurations {
 
     @Bean
     fun customerActivateUseCase(customerRepository: CustomerRepository) = ActivateCustomerUseCase(customerRepository = customerRepository)
+
+    @Bean
+    fun activateSubscriptionUseCase(customerRepository: CustomerRepository) =
+        ActivateSubscriptionUseCase(customerRepository = customerRepository)
 }
