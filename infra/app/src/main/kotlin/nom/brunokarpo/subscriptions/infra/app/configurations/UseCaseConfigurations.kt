68c11d3b22779cbd600cf1766d542f0d2886f167
Bruno Nogueira <ti.brunonogueira@gmail.com>
2025-11-01 14:18:47 -0300

    fix: Fix spring build

--- infra/app/src/main/kotlin/nom/brunokarpo/subscriptions/infra/app/configurations/UseCaseConfigurations.kt
+++ infra/app/src/main/kotlin/nom/brunokarpo/subscriptions/infra/app/configurations/UseCaseConfigurations.kt
@@ -3,6 +3,7 @@ package nom.brunokarpo.subscriptions.infra.app.configurations
 import nom.brunokarpo.subscriptions.application.customer.ActivateCustomerUseCase
 import nom.brunokarpo.subscriptions.application.customer.ActivateSubscriptionUseCase
 import nom.brunokarpo.subscriptions.application.customer.CreateNewCustomerUseCase
+import nom.brunokarpo.subscriptions.application.customer.DeactivateCustomerUseCase
 import nom.brunokarpo.subscriptions.application.customer.RetrieveSubscriptionsByStatusUseCase
 import nom.brunokarpo.subscriptions.application.customer.SubscribeProductToCustomerUseCase
 import nom.brunokarpo.subscriptions.application.product.CreateNewProductUseCase
@@ -37,4 +38,8 @@ class UseCaseConfigurations {
     @Bean
     fun activateSubscriptionUseCase(customerRepository: CustomerRepository) =
         ActivateSubscriptionUseCase(customerRepository = customerRepository)
+
+    @Bean
+    fun deactivateCustomerUseCase(customerRepository: CustomerRepository) =
+        DeactivateCustomerUseCase(customerRepository = customerRepository)
 }
